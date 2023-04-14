3e0f2f627b379807057b7cfecf040c081c2a527f
BillStephens2022 <two4onebill@yahoo.com>
2023-04-14 14:33:33 -0400

    fixed bug

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def home():
 
 
 @app.route('/<int:guess>')
-def guess():
+def guess_number(guess):
     if guess > random_number:
         return "<h1 style='color: violet'>Too High! Try Again...</h1>" \
                "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
